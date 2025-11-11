a9d0765eaf65783e11657b8ae6ba2387d7d2beeb
Haldamird212 <david.ordonez@outlook.cl>
2025-11-11 14:31:43 -0300

    creando funcionalidad de boton agregar al carrito mediante el uso de un callback a una funcion

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,5 +1,7 @@
 export default function Guitar({guitar}) {
-  console.log(guitar)
+  const handleClick = (guitar) =>{
+    console.log(guitar.id)
+  }
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
       <div className="col-4">
@@ -15,7 +17,8 @@ export default function Guitar({guitar}) {
          {guitar.description}
         </p>
         <p className="fw-black text-primary fs-3">${guitar.price}</p>
-        <button type="button" className="btn btn-dark w-100">
+        <button type="button" className="btn btn-dark w-100"
+        onClick={() => handleClick(guitar)}>
           Agregar al Carrito
         </button>
       </div>
